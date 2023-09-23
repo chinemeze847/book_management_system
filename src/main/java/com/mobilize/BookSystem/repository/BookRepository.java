8a6a357d0869ab694d12bb3f56879e5127899834
chinemeze847 <thankgodchinemeze@gmail.com>
2023-09-23 21:47:57 +0100

    added comments repository and service interface

--- src/main/java/com/mobilize/BookSystem/repository/BookRepository.java
+++ src/main/java/com/mobilize/BookSystem/repository/BookRepository.java
@@ -10,14 +10,42 @@ import org.springframework.stereotype.Repository;
 
 import com.mobilize.BookSystem.model.Book;
 
+/**
+ * Repository interface for accessing and managing book data in the database.
+ */
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+	/**
+	 * Find all books with pagination support.
+	 *
+	 * @param pageable Pagination information.
+	 * @return A page of books.
+	 */
 	Page<Book> findAll(Pageable pageable);
 
+	/**
+	 * Find books by title, ignoring case.
+	 *
+	 * @param title The title to search for.
+	 * @return A list of books matching the title.
+	 */
 	List<Book> findByTitleContainingIgnoreCase(String title);
 
+	/**
+	 * Find books by author, ignoring case.
+	 *
+	 * @param author The author to search for.
+	 * @return A list of books written by the author.
+	 */
 	List<Book> findByAuthorContainingIgnoreCase(String author);
 
+	/**
+	 * Find books by title and author, ignoring case.
+	 *
+	 * @param title  The title to search for.
+	 * @param author The author to search for.
+	 * @return A list of books matching both the title and author.
+	 */
 	List<Book> findByTitleContainingIgnoreCaseAndAuthorContainingIgnoreCase(String title, String author);
 }
--- src/main/java/com/mobilize/BookSystem/service/BookService.java
+++ src/main/java/com/mobilize/BookSystem/service/BookService.java
@@ -1,5 +1,6 @@
 package com.mobilize.BookSystem.service;
 
+
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -9,16 +10,57 @@ import com.mobilize.BookSystem.dto.BookRequestDTO;
 import com.mobilize.BookSystem.dto.BookUpdateDTO;
 import com.mobilize.BookSystem.model.Book;
 
+/**
+ * Service interface for managing book-related operations.
+ */
 public interface BookService {
+
+	/**
+	 * Create a new book.
+	 *
+	 * @param bookRequest The data for the new book.
+	 * @return The created book.
+	 */
 	Object createBook(BookRequestDTO bookRequest);
 
+	/**
+	 * Retrieve a paginated list of all books.
+	 *
+	 * @param pageable Pagination information.
+	 * @return A page of books.
+	 */
 	Page<Book> getAllBooks(Pageable pageable);
 
+	/**
+	 * Update an existing book.
+	 *
+	 * @param id          The ID of the book to update.
+	 * @param updatedBook The updated book data.
+	 * @return The updated book.
+	 */
 	Book updateBook(Long id, BookUpdateDTO updatedBook);
 
+	/**
+	 * Delete a book by its ID.
+	 *
+	 * @param bookId The ID of the book to delete.
+	 */
 	void deleteBook(Long bookId);
 
+	/**
+	 * Retrieve a book by its ID.
+	 *
+	 * @param id The ID of the book to retrieve.
+	 * @return The book with the specified ID.
+	 */
 	Book getBookById(Long id);
 
+	/**
+	 * Search for books by title and/or author, ignoring case.
+	 *
+	 * @param title  The title to search for (can be null).
+	 * @param author The author to search for (can be null).
+	 * @return A list of books matching the search criteria.
+	 */
 	List<Book> searchBooks(String title, String author);
 }
