5eccd2d5d68c2a4f601e6fe03b8537edef09b4c8
chinemeze847 <thankgodchinemeze@gmail.com>
2023-09-23 21:54:50 +0100

    added comments to book controller advice

--- src/main/java/com/mobilize/BookSystem/exception/adviser/BookControllerAdvice.java
+++ src/main/java/com/mobilize/BookSystem/exception/adviser/BookControllerAdvice.java
@@ -13,8 +13,18 @@ import com.mobilize.BookSystem.exception.BookValidationException;
 import com.mobilize.BookSystem.exception.ErrorMessage;
 import com.mobilize.BookSystem.exception.InvalidSearchParametersException;
 
+/**
+ * Controller advice class to handle exceptions globally for the BookController.
+ */
 @ControllerAdvice
 public class BookControllerAdvice {
+
+	/**
+	 * Exception handler for BookNotFoundException.
+	 * @param ex The BookNotFoundException instance.
+	 * @param request The WebRequest.
+	 * @return ResponseEntity containing error details and HTTP status NOT_FOUND.
+	 */
 	@ExceptionHandler(BookNotFoundException.class)
 	public ResponseEntity<ErrorMessage> bookNotFoundException(BookNotFoundException ex, WebRequest request) {
 		ErrorMessage message = new ErrorMessage(
@@ -26,6 +36,12 @@ public class BookControllerAdvice {
 		return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
 	}
 
+	/**
+	 * Exception handler for BookValidationException.
+	 * @param ex The BookValidationException instance.
+	 * @param request The WebRequest.
+	 * @return ResponseEntity containing error details and HTTP status BAD_REQUEST.
+	 */
 	@ExceptionHandler(BookValidationException.class)
 	public ResponseEntity<ErrorMessage> bookValidationException(BookValidationException ex, WebRequest request) {
 		ErrorMessage message = new ErrorMessage(
@@ -37,6 +53,12 @@ public class BookControllerAdvice {
 		return new ResponseEntity<ErrorMessage>(message, HttpStatus.BAD_REQUEST);
 	}
 
+	/**
+	 * Exception handler for InvalidSearchParametersException.
+	 * @param ex The InvalidSearchParametersException instance.
+	 * @param request The WebRequest.
+	 * @return ResponseEntity containing error details and HTTP status BAD_REQUEST.
+	 */
 	@ExceptionHandler(InvalidSearchParametersException.class)
 	public ResponseEntity<ErrorMessage> invalidSearchParametersException(InvalidSearchParametersException ex, WebRequest request) {
 		ErrorMessage message = new ErrorMessage(
@@ -48,6 +70,12 @@ public class BookControllerAdvice {
 		return new ResponseEntity<ErrorMessage>(message, HttpStatus.BAD_REQUEST);
 	}
 
+	/**
+	 * Exception handler for generic exceptions.
+	 * @param ex The Exception instance.
+	 * @param request The WebRequest.
+	 * @return ResponseEntity containing error details and HTTP status INTERNAL_SERVER_ERROR.
+	 */
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request) {
 		ErrorMessage message = new ErrorMessage(
